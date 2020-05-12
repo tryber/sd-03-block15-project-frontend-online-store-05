8177cabfb77fa91dd822a133aba4054ee549fe55
gabryel <gabryel.ryba@gmail.com>
2020-05-12 20:01:39 -0300

    teste avaliador

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,7 +3,7 @@ import React from 'react';
 class ProductList extends React.Component {
   render() {
     return (
-      <div>
+      <div className="product-wrapper">
         <p
           data-testid="home-initial-message"
         >
