2f5b504cda1b46e50e4cef6b451d0422f4bfecdb
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-05-12 15:53:19 -0400

    App Fix CC

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import * as api from './services/api';
 
 function App() {
-  api.getCategories().then(categories => { console.log(categories) });
+  api.getCategories()
+    .then((categories) => { console.log(categories); });
   return (
     <div className="App" />
   );
