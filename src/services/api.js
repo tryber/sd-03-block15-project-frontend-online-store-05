70de113c996132daeb0b99da456880cf2b72a43a
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-05-12 15:47:29 -0400

    Api.js Fix CC

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 export async function getCategories() {
-  return fetch(`https://api.mercadolibre.com/sites/MLB/categories`)
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
     .then((response) => response.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+  return fetch('https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}')
     .then((response) => response.json());
 }
