b6c2349c8b9139933d2d432c1942d59076a6b56c
gabryel <gabryel.ryba@gmail.com>
2020-05-13 17:51:27 -0300

    Added categorie names to categories page

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import * as api from '../services/api';
 
 class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { categories: '' };
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.renderCategoriesList = this.renderCategoriesList.bind(this);
+  }
+
+  componentDidMount() {
+    const { categories } = this.state;
+    api.getCategories()
+      .then((results) => this.setState({ categories: results }))
+      .then(() => console.log(categories));
+  }
+
+  renderCategoriesList() {
+    const { categories } = this.state;
+    if (typeof categories === 'object') {
+      return categories.map((object) => <li key={object.name}>{object.name}</li>);
+    }
+    return (<li>Carregando Categorias</li>);
+  }
+
   render() {
     return (
       <div>
         <h1 data-testid="category">Categorias:</h1>
+        <ul>
+          {this.renderCategoriesList()}
+        </ul>
       </div>
     );
   }
