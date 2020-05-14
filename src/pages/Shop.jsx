b2236ca0e3d8b33e5b7ba49538075bbf97704988
gabryel <gabryel.ryba@gmail.com>
2020-05-14 19:49:29 -0300

    corrigindo requisito 3 e codeclimate

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,6 +5,11 @@ class Shop extends Component {
   render() {
     return (
       <div className="Shop">
+        <img
+          src="https://w7.pngwing.com/pngs/387/168/png-transparent-shopping-cart-button-graphy-earring-shopping-cart-angle-rectangle-logo.png"
+          alt="shopping cart button"
+          data-testid="shopping-cart-button"
+        />
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
       </div>
     );
