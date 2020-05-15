dc356cc0e7e05157697171cb8ddb45274fadc2a7
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-05-15 05:37:03 -0400

    Fix CSS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Details from './pages/Details';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Shop" component={Shop} />
-      <Route exact path="/product/:id" component={Details} />
-    </Switch>
-  </Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Shop" component={Shop} />
+        <Route exact path="/product/:id" component={Details} />
+      </Switch>
+    </Router>
   );
 }
 
