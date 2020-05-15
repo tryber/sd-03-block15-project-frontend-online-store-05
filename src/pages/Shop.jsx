f2499bef2b246f4f78af88c76e99faeb859a2f90
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-05-15 18:22:37 -0400

    Fix CC

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -8,8 +8,8 @@ class Shop extends Component {
     return (
       <div className="ShopCart" data-testid="shopping-cart-button">
         <Link to="/">Voltar</Link>
-        <p 
-          className="MessageEmpt" 
+        <p
+          className="MessageEmpt"
           data-testid="shopping-cart-empty-message"
         >
           Seu carrinho está vazio</p>
